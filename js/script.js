50612bdc2eae143e0a74b6449328859d16946462
Eli Sagy <sagyeli@gmail.com>
2015-03-24 22:01:52 +0200

    Pages 1 & 2

--- js/script.js
+++ js/script.js
@@ -1,34 +1,35 @@
 var handShakeApp = angular.module('handShakeApp', ['ngRoute']);
 
 handShakeApp.config(function($routeProvider) {   
-    $routeProvider
-        .when('/', {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'page1'
+		})
+		.when('/page1', {
             templateUrl: 'page1'
         })
         .when('/page2', {
-            templateUrl: 'page2'
-        })
-        .when('/page3', {
-            templateUrl: 'page3'
-        })
-        .when('/page4', {
-            templateUrl: 'page4'
-        })
-        .when('/page5', {
-            templateUrl: 'page5'
-        })
-        .when('/page6', {
-            templateUrl: 'page6'
-        })
-        .when('/page7', {
-            templateUrl: 'page7'
-        })
-        .when('/page8', {
-            templateUrl: 'page8'
-        })
-        .when('/page9', {
-            templateUrl: 'page9'
-        });
+			templateUrl: 'page2'
+		})
+		.when('/page3', {
+			templateUrl: 'page3'
+		})
+		.when('/page4', {
+			templateUrl: 'page4'
+		})
+		.when('/page5', {
+			templateUrl: 'page5'
+		})
+		.when('/page6', {
+			templateUrl: 'page6'
+		})
+		.when('/page7', {
+			templateUrl: 'page7'
+		})
+		.when('/page8', {
+			templateUrl: 'page8'
+		})
+		.when('/page9', {
+			templateUrl: 'page9'
+		});
 });
-
-

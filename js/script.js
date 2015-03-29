2efc3fd4439be9a2653b3f823e5cb60f2fdb9762
Eli Sagy <sagyeli@gmail.com>
2015-03-29 03:33:52 +0300

    Non-complete page 6

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ var qrcode_write;
 
 var handShakeApp = angular.module('handShakeApp', ['ngRoute']);
 
-handShakeApp.run(function($rootScope){
+handShakeApp.run(function($rootScope, $http){
 	$rootScope.root = $rootScope; 
 	$rootScope.privateKey = '';
 	$rootScope.publicKey = '';
@@ -24,6 +24,29 @@ handShakeApp.run(function($rootScope){
     $rootScope.insertTargetAddressAndAmount = function() {
         $rootScope.publicKey = $('#input_page_5_1').val();
         $rootScope.amount = $('#input_page_5_2').val();
+
+        var txFee = 10000;
+        var txTargetValue = parseFloat($rootScope.amount) * 100000000;
+
+        $http.get('https://blockchain.info/unspent?active=' + $rootScope.publicColdStorageKey + '&cors=true')
+            .success(function(data, status, headers, config) {
+                var tx = new Bitcoin.Transaction();
+
+                var totalUnspentsValue = 0;
+                for (var i = 0 ; i < data.unspent_outputs.length ; i++) {
+                    tx.addInput(data.unspent_outputs[i].tx_hash, data.unspent_outputs[i].tx_index);
+                    totalUnspentsValue += data.unspent_outputs[i].value;
+                }
+                tx.addOutput({ hash: $rootScope.publicKey }, txTargetValue);
+
+                var txChangeValue = totalUnspentsValue - txTargetValue - txFee;
+                tx.addOutput({ hash: $rootScope.publicColdStorageKey }, txChangeValue);
+                
+                // Eli says: from this point I'm a bit clueless... how exactliy can we ust get the transaction unsigned hash???
+
+                var transactionUnsignedHash = 'blablabla';
+                qrcode_write.makeCode(transactionUnsignedHash);
+            });
     };
 });
 
